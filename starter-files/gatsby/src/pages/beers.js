756be2cda96f16d5ad5719033c13fe1eb4eb33c4
Rebecca Hong <rebeccahongsf@gmail.com>
2021-01-04 20:00:00 -0800

    add global Layout

--- starter-files/gatsby/src/pages/beers.js
+++ starter-files/gatsby/src/pages/beers.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function BeersPage() {
   return (
-    <div>
+    <>
       <p>Hey! This is the Beers page</p>
-    </div>
+    </>
   )
 }
--- starter-files/gatsby/src/pages/index.js
+++ starter-files/gatsby/src/pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Layout from '../components/Layout';
 
 export default function HomePage() {
   return (
-    <div>
+    <>
       <p>Hey! This is the homepage</p>
-    </div>
-  )
+    </>
+  );
 }
