83747776eac72936743883ad088ad1a8a74ccec2
Rebecca Hong <rebeccahongsf@gmail.com>
2021-01-04 21:18:08 -0800

    add global styles

--- starter-files/gatsby/gatsby-ssr.js
+++ starter-files/gatsby/gatsby-ssr.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from './src/components/Layout';
+import 'normalize.css';
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
