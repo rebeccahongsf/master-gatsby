b9b79bcbbbf07f2fbb08348c1a59e87ad95c11e8
Rebecca Hong <rebeccahongsf@gmail.com>
2021-01-11 17:15:21 -0800

    style(nav): add styling to nav component

--- starter-files/gatsby/gatsby-browser.js
+++ starter-files/gatsby/gatsby-browser.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Layout from './src/components/Layout';
-import 'normalize.css';
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
