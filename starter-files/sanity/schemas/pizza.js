f7811ca5d5576a0d57b88f606de14840166019d5
Rebecca Hong <rebeccahongsf@gmail.com>
2021-01-18 10:02:35 -0800

    feat(sanity): initialize sanity backend database

---
+++ starter-files/sanity/schemas/pizza.js
@@ -0,0 +1,44 @@
+import { MdLocalPizza as icon } from 'react-icons/md';
+
+export default {
+  // computer name
+  name: 'pizza',
+  // visible title
+  title: 'Pizzas',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'Name of pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price of the pizza in cents',
+      validation: (Rule) => Rule.min(1000).max(50000),
+      // TODO: Add custom input component
+    },
+  ],
+};
